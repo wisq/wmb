ed08172db4783fe81624e716e299678a0aa5905b
Adrian Irving-Beer <wisq@wisq.net>
2012-09-26 16:17:45 -0400

    Basic DB building.

--- lib/wmb.rb
+++ lib/wmb.rb
@@ -1,6 +1,7 @@
 require 'yaml'
 require 'pathname'
 require 'forwardable'
+require 'tempfile'
 
 module WMB
   class Rules
@@ -18,7 +19,7 @@ module WMB
           path  = rules.path + part
           rules = (rules[part] ||= Rules.new(path))
         end
-        rules.mode = mode
+        rules.mode = mode.to_sym
       end
 
       root.prune!
@@ -27,6 +28,8 @@ module WMB
 
     extend Forwardable
     def_delegator :@kids, :[]
+    def_delegator :@kids, :each
+    def_delegator :@kids, :empty?
 
     attr_accessor :mode
     attr_reader :path
@@ -41,12 +44,133 @@ module WMB
       @kids[name] = rules
     end
 
+    def watch?
+      @mode == :watch
+    end
+
+    def unwatch?
+      @mode && !watch?
+    end
+
+    def blocks_watch?
+      unwatch? && @kids.empty?
+    end
+
     # Delete children who have the same mode as their closest ancestor.
     def prune!(parent_mode = nil)
       parent_mode = self.mode if self.mode
 
       @kids.delete_if { |_, kid| kid.mode == parent_mode } unless parent_mode.nil?
       @kids.values.each { |kid| kid.prune!(parent_mode) }
+    end
+  end
+
+  class Watcher
+    class Node
+      def self.create(path)
+        stat = path.stat
+        cls = (stat.directory? ? DirNode : FileNode)
+        cls.new(path)
+      rescue Errno::ENOENT
+        NilNode.new(path)
+      end
+
+      def self.root
+        DirNode.new(Pathname.new('/'))
+      end
+
+      def initialize(path)
+        @path = path
+      end
+    end
+
+    class NilNode < Node
+      def watch(rules); end
+      def travel(rules); end
+
+      def nil?
+        true
+      end
+    end
+
+    class FileNode < Node
+      def watch(rules)
+        stat = @path.stat
+        @size  = stat.size
+        @mtime = stat.mtime
+      end
+
+      def travel(rules)
+        # nothing to do
+      end
+    end
+
+    class DirNode < Node
+      def initialize(path)
+        super(path)
+        @kids = {}
+      end
+
+      def travel(rules)
+        rules.each do |path, rule|
+          if !rule.blocks_watch?
+            kid = Node.create(rule.path)
+            if rule.watch?
+              kid.watch(rule)
+            else
+              kid.travel(rule)
+            end
+            @kids[path] = kid unless kid.nil?
+          end
+        end
+      end
+
+      def watch(rules)
+        @path.each_child do |subpath|
+          key  = subpath.basename.to_s
+          rule = rules[key] if rules
+
+          kid = Node.create(subpath)
+          if !rule
+            kid.watch(nil)
+          elsif rule.unwatch?
+            if rule.empty?
+              kid = nil
+            else
+              kid.travel(rule)
+            end
+          else
+            kid.watch(rule)
+          end
+
+          @kids[key] = kid unless kid.nil?
+        end
+      end
+    end
+
+    def initialize(rules)
+      @rules = rules
+    end
+
+    def load(file)
+      @db = YAML.load_file(file)
+    end
+
+    def save(file)
+      fh = Tempfile.open(File.basename(file), File.dirname(file))
+      fh.puts @db.to_yaml
+      fh.close
+      fh.rename(file)
+    ensure
+      fh.close! if fh
+    end
+
+    def run
+      new_db = Node.root
+      new_db.travel(@rules)
+      # prepare a change report here
+      pp new_db
+      @db = new_db
     end
   end
 end
